702ce56c553a4ac987e8bc72ed77a640e6485317
BruceMacAdam1303 <zorichtarin@gmail.com>
2025-11-15 11:20:00 +0800

    Add FHEVM integration and MetaMask hooks

---
+++ frontend/abi/SupplyChainTrackingAddresses.ts
@@ -0,0 +1,12 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+*/
+export const SupplyChainTrackingAddresses = {
+  "11155111": {
+    "address": "0x624F32C78d314e3b0bE173031a0E34D5B8D0D56C",
+    "chainId": 11155111,
+    "chainName": "sepolia"
+  }
+};
